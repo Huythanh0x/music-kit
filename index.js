e738999b1e1175880a877a44f43fe7daa4c0d819
huythanh0x <huythanh0x@gmail.com>
2021-05-25 00:33:10 +0700

    fix error when press random key

--- index.js
+++ index.js
@@ -12,38 +12,52 @@ document.addEventListener('keydown', function(event) {
 	playSound(event.key);
 	animationEffect(event.key);
 });
-var audio = new Audio('sounds/f.mp3');
+var audio;
+audio.addEventListener('ended', function() {
+    this.currentTime = 0;
+    this.play();
+}, false);
 function playSound(key) {
-	audio.pause();
-	audio.currentTime = 0;
+	list_key = ['f','a','s','d','j','k','l']
+	if(audio && list_key.includes(key)){
+		audio.pause();
+		audio.currentTime = 0;
+		audio.loop = true;	
+	}
 	switch (key) {
 		case 'f':
 			audio = new Audio('sounds/f.mp3');
+			audio.play();
 			break;
 		case 'a':
 			audio = new Audio('sounds/a.mp3');
+			audio.play();
 			break;
 		case 's':
 			audio = new Audio('sounds/s.mp3');
+			audio.play();
 			break;
 		case 'd':
 			audio = new Audio('sounds/d.mp3');
+			audio.play();
 			break;
 		case 'j':
 			audio = new Audio('sounds/j.mp3');
+			audio.play();
 			break;
 		case 'k':
 			audio = new Audio('sounds/k.mp3');
+			audio.play();
 			break;
 		case 'l':
 			audio = new Audio('sounds/l.mp3');
-
+			audio.play();
 			break;
 		default:
 			console.log('What is happing?');
 			break;
 	}
-	audio.play();
+	
 }
 function animationEffect(key) {
 	document.querySelector('.' + key).classList.add('pressed');
