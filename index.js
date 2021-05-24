1c2cf38ce711055b19bfb860039bd82bea0e9268
huythanh0x <huythanh0x@gmail.com>
2021-05-24 23:54:04 +0700

    can play sepratedly

--- index.js
+++ index.js
@@ -12,46 +12,42 @@ document.addEventListener('keydown', function(event) {
 	playSound(event.key);
 	animationEffect(event.key);
 });
-
+var audio = new Audio('sounds/f.mp3');
 function playSound(key) {
+	audio.pause();
+	audio.currentTime = 0;
 	switch (key) {
 		case 'f':
-			var audio = new Audio('sounds/f.mp3');
-			audio.play();
+			audio = new Audio('sounds/f.mp3');
 			break;
 		case 'a':
-			var audio = new Audio('sounds/a.mp3');
-			audio.play();
+			audio = new Audio('sounds/a.mp3');
 			break;
 		case 's':
-			var audio = new Audio('sounds/s.mp3');
-			audio.play();
+			audio = new Audio('sounds/s.mp3');
 			break;
 		case 'd':
-			var audio = new Audio('sounds/d.mp3');
-			audio.play();
+			audio = new Audio('sounds/d.mp3');
 			break;
 		case 'j':
-			var audio = new Audio('sounds/j.mp3');
-			audio.play();
+			audio = new Audio('sounds/j.mp3');
 			break;
 		case 'k':
-			var audio = new Audio('sounds/k.mp3');
-			audio.play();
+			audio = new Audio('sounds/k.mp3');
 			break;
 		case 'l':
-			var audio = new Audio('sounds/l.mp3');
-			audio.play();
+			audio = new Audio('sounds/l.mp3');
+
 			break;
 		default:
 			console.log('What is happing?');
 			break;
-	}	
+	}
+	audio.play();
 }
 function animationEffect(key) {
-    document.querySelector("." + key).classList.add("pressed");
-    setTimeout(function() {
-        document.querySelector("." + key).classList.remove("pressed");
-    }, 100);
-    
-}   
+	document.querySelector('.' + key).classList.add('pressed');
+	setTimeout(function() {
+		document.querySelector('.' + key).classList.remove('pressed');
+	}, 100);
+}
